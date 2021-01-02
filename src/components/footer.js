a8e145fb796ee7ac60f92ddf9efb8182d5c82f2a
Siwoo Baek <whitelatte46@gmail.com>
2021-01-02 23:03:46 +0900

    :lipstick: Footer 스타일 수정

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,21 +1,37 @@
 import React from "react";
 
 const linkStyle = {
-    color: `Coral`,
-    textDecoration: `none`,
+  color: `Coral`,
+  textDecoration: `none`,
 };
 
 const Footer = () => (
-    <footer style={{ marginTop: `5rem`, textAlign: `center`}}>
-        <p style={{ color: `LightGray`,}}>Спасибо. Хорошего дня!</p>
-        <div>
-            © {new Date().getFullYear()}, Built with {` `}
-            <a style={linkStyle} href="https://www.gatsbyjs.com" target="_blank" rel="noreferrer">Gatsby</a>
-            , {` `} Posted by {` `}
-            <a style={linkStyle} href="https://github.com/lattepapa" target="_blank" rel="noreferrer">lattepapa</a>
-        </div>
-        <br />
-    </footer>
-)
+  <footer
+    style={{ marginTop: `5rem`, marginBottom: `5rem`, textAlign: `center` }}
+  >
+    <p style={{ color: `LightGray` }}>Спасибо. Хорошего дня!</p>
+    <div>
+      © {new Date().getFullYear()}, Built with {` `}
+      <a
+        style={linkStyle}
+        href="https://www.gatsbyjs.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Gatsby
+      </a>
+      , {` `} Posted by {` `}
+      <a
+        style={linkStyle}
+        href="https://github.com/lattepapa"
+        target="_blank"
+        rel="noreferrer"
+      >
+        lattepapa
+      </a>
+    </div>
+    <br />
+  </footer>
+);
 
 export default Footer;
